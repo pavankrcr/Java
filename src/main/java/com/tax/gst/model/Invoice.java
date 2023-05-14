d29f82e5ddb3ad3dcb78f75e1462733736ef9883
Pavank <mail@pavank.in>
2023-05-14 17:00:21 +0530

    t

--- src/main/java/com/tax/gst/model/Invoice.java
+++ src/main/java/com/tax/gst/model/Invoice.java
@@ -2,10 +2,8 @@ package com.tax.gst.model;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
 public class Invoice {
